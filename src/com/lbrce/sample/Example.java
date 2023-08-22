5a7129bd3c1cb36775793e364bc8f475e05ce5a1
ML Lab <ML Lab@MLLAB01>
2023-08-22 10:49:03 +0530

    First Commit

---
+++ src/com/lbrce/sample/Example.java
@@ -0,0 +1,14 @@
+package com.lbrce.sample;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		srinu();
+	}
+	public static void srinu()
+	{
+		System.out.println("My Name is Srinu");
+	}
+
+}
